9f29bb067b9e0ccd8d507b099c152baf37f89eb9
Jennifer Brisbane <jmbrisbane01@gmail.com>
2016-01-09 00:38:54 -0500

    Initial Commit

---
+++ Browser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Browser
+//
+//  Created by Jennifer Brisbane on 1/9/16.
+//  Copyright © 2016 jellroll. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
